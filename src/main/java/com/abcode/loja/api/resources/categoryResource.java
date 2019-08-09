3c4fe51a0b07d62b2392d93ba2ef269de2f6f66a
Arthur Barbosa <arthurbruno03@gmail.com>
2019-08-09 16:06:05 -0400

    first class domain

--- src/main/java/com/abcode/loja/api/resources/categoryResource.java
+++ src/main/java/com/abcode/loja/api/resources/categoryResource.java
@@ -1,15 +1,27 @@
 package com.abcode.loja.api.resources;
 
+import com.abcode.loja.api.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class categoryResource {
 
     @GetMapping
-    public String listar(){
-         return "Rest ta funcionando";
+    public List<Category> listar(){
+        Category cat = new Category(1L, "Informática");
+        Category cat1 = new Category(2L,"Escritório");
+
+        List<Category> lista = new ArrayList<>();
+
+        lista.add(cat);
+        lista.add(cat1);
+
+         return lista;
     }
 }
